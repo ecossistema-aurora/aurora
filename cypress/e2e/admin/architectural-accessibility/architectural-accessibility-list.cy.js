80e820e6f035dd121d2a3e90e7347914db8bffca
Sara Camilo <saraccamilo@gmail.com>
2025-02-27 10:53:40 -0300

    create list architectural accessibility

---
+++ cypress/e2e/admin/architectural-accessibility/architectural-accessibility-list.cy.js
@@ -0,0 +1,26 @@
+describe('Teste de navegação e validação da página de Acessibilidade Arquitetônica', () => {
+    it('Deve acessar a página de Acessibilidade Arquitetônica a partir de Oportunidades após login', () => {
+        cy.visit('/');
+
+        cy.contains('Entrar').click();
+
+        cy.url().should('include', '/login');
+
+        cy.login('saracamilo@example.com', 'Aurora@2024');
+
+        cy.url().should('include', '/');
+
+        cy.get('.navbar').contains('Sara').should('be.visible');
+        cy.get('.navbar').contains('Sara').click();
+
+        cy.contains('Minhas Oportunidades', { timeout: 10000 }).should('be.visible').click();
+
+        cy.url({ timeout: 10000 }).should('include', '/painel/oportunidades');
+
+        cy.scrollTo('bottom');
+
+        cy.contains('Acessibilidade Arquitetônica', { timeout: 10000 }).should('be.visible').click();
+
+        cy.get('table', { timeout: 10000 }).should('be.visible');
+    });
+});
