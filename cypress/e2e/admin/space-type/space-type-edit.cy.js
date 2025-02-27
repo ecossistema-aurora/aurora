79d289514726ed5bc820ecdee3d31d69987dba3f
TalysonSoares <talysonsoares40@gmail.com>
2025-02-27 10:48:37 -0300

    Create Edit Action for Space Type

---
+++ cypress/e2e/admin/space-type/space-type-edit.cy.js
@@ -0,0 +1,31 @@
+describe('Teste para Editar Tipo de Espaço', () => {
+    beforeEach(() => {
+        cy.viewport(1920, 1080);
+        cy.login('talysonsoares@example.com', 'Aurora@2024');
+        cy.visit('/painel/admin/tipo-de-espaco');
+    });
+
+    it('Editar um tipo de espaço existente', () => {
+        cy.get('tbody > :nth-child(1) > :nth-child(3) > .btn').click();
+
+        cy.get('[data-cy="name"]').should('exist').and('have.value', 'Casa de Show');
+
+        cy.get('[data-cy="name"]').clear()
+        cy.get('[data-cy="name"]').type('Anfiteatro');
+        cy.get('[type="submit"]').click();
+
+        cy.get('.success.snackbar').contains('O Tipo de Espaço foi atualizado').should('be.visible');
+
+        cy.contains('Anfiteatro').should('be.visible');
+    });
+
+    // it('Tenta editar com um nome inválido', () => {
+    //     cy.get('tbody > :nth-child(1) > :nth-child(2)').click();
+    //
+    //     cy.get('[data-cy="name"]').clear()
+    //     cy.get('[data-cy="name"]').type('P');
+    //     cy.get('[type="submit"]').click();
+    //
+    //     cy.get('.danger.snackbar').contains('O valor é muito curto. Deveria de ter 2 caracteres ou mais.').should('be.visible');
+    // });
+});
