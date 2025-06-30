d9514e7eb0369d4266a505d7a0806bbd3ef1611f
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2025-06-30 08:18:41 -0300

    Remove regmel waste files and configurations (#356)

--- src/Environment/ConfigEnvironment.php
+++ src/Environment/ConfigEnvironment.php
@@ -13,10 +13,6 @@ class ConfigEnvironment
         $dirname = dirname(__DIR__, 2).'/config/environment';
         $filename = "{$dirname}/aurora.yaml";
 
-        if (true === file_exists("{$dirname}/aurora.regmel.yaml")) {
-            $filename = "{$dirname}/aurora.regmel.yaml";
-        }
-
         if (true === file_exists("{$dirname}/aurora.local.yaml")) {
             $filename = "{$dirname}/aurora.local.yaml";
         }
@@ -26,12 +22,5 @@ class ConfigEnvironment
         }
 
         return Yaml::parseFile($filename);
-    }
-
-    public static function isRegmel(): bool
-    {
-        $dirname = dirname(__DIR__, 2).'/config/environment';
-
-        return file_exists("{$dirname}/aurora.regmel.yaml");
     }
 }
