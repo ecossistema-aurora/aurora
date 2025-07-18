d8d9e1619614b51a371e7951e04522b38fe099c4
Sara Camilo <saraccamilo@gmail.com>
2025-07-18 15:57:54 -0300

    report deleted status

--- cypress/aurora/e2e/admin/agents/agent-list.cy.js
+++ cypress/aurora/e2e/admin/agents/agent-list.cy.js
@@ -8,9 +8,14 @@ describe('Painel de Controle - Página de listar Agentes', () => {
     it('Garante que a página de Agentes existe e funciona', () => {
         cy.get('h2').contains('Meus Agentes').should('be.visible');
 
-        cy.get('tbody > tr > :nth-child(1)').contains('Henrique').should('be.visible');
-        cy.get('tbody >  tr > :nth-child(2)').contains('11/07/2024 10:49:00').should('be.visible');
+        cy.get('tbody > tr > :nth-child(1)')
+            .contains('Henrique')
+            .should('be.visible');
+
+        cy.get('tbody > tr > :nth-child(3)')
+            .contains('11/07/2024 10:49:00')
+            .should('be.visible');
 
         cy.get('.table').should('not.contain', 'Excluir');
     });
-})
+});
