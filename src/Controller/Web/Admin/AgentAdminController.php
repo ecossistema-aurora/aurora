ae540fd36d0f7b70a81f39394533fa17b89d48f2
Henrique Lopes Lima <38414109+henriquelopeslima@users.noreply.github.com>
2026-02-06 18:13:15 -0300

    Improve agent info and address on create form (#475)

--- src/Controller/Web/Admin/AgentAdminController.php
+++ src/Controller/Web/Admin/AgentAdminController.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace App\Controller\Web\Admin;
 
 use App\DocumentService\AgentTimelineDocumentService;
+use App\Entity\Agent;
+use App\Entity\AgentAddress;
+use App\Entity\City;
 use App\Enum\FlashMessageTypeEnum;
 use App\Enum\UserRolesEnum;
 use App\Exception\ValidatorException;
+use App\Repository\Interface\AddressRepositoryInterface;
 use App\Service\Interface\AgentServiceInterface;
+use App\Service\Interface\StateServiceInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -32,6 +38,9 @@ class AgentAdminController extends AbstractAdminController
         private readonly AgentTimelineDocumentService $documentService,
         private readonly JWTTokenManagerInterface $jwtManager,
         private readonly TranslatorInterface $translator,
+        private readonly StateServiceInterface $stateService,
+        private readonly AddressRepositoryInterface $addressRepository,
+        private readonly EntityManagerInterface $entityManager,
         private readonly Security $security,
     ) {
     }
@@ -55,6 +64,8 @@ class AgentAdminController extends AbstractAdminController
         if (false === $request->isMethod(Request::METHOD_POST)) {
             return $this->render(self::VIEW_ADD, [
                 'form_id' => self::CREATE_FORM_ID,
+                'states' => $this->stateService->list(),
+                'cities' => [],
             ]);
         }
 
@@ -63,14 +74,14 @@ class AgentAdminController extends AbstractAdminController
         $errors = [];
 
         try {
-            $this->service->create([
-                'id' => Uuid::v4(),
-                'name' => $request->get('name'),
-                'shortBio' => $request->get('shortBio'),
-                'longBio' => $request->get('shortBio'),
-                'culture' => false,
-                'user' => $this->security->getUser()->getId(),
-            ]);
+            $agentData = $this->extractAgentDataFromRequest($request);
+            $addressData = $this->extractAddressDataFromRequest($request);
+
+            $agent = $this->service->create($agentData);
+
+            if (!empty($addressData)) {
+                $this->createAgentAddress($agent, $addressData);
+            }
 
             $this->addFlash(FlashMessageTypeEnum::SUCCESS->value, $this->translator->trans('view.agent.message.created'));
         } catch (ValidatorException $exception) {
@@ -83,10 +94,81 @@ class AgentAdminController extends AbstractAdminController
             return $this->render(self::VIEW_ADD, [
                 'errors' => $errors,
                 'form_id' => self::CREATE_FORM_ID,
+                'states' => $this->stateService->list(),
+                'cities' => [],
             ]);
         }
 
         return $this->redirectToRoute('admin_agent_list');
+    }
+
+    private function extractAgentDataFromRequest(Request $request): array
+    {
+        return [
+            'id' => Uuid::v4(),
+            'name' => $request->get('name'),
+            'shortBio' => $request->get('shortBio'),
+            'longBio' => $request->get('shortBio'),
+            'culture' => false,
+            'user' => $this->security->getUser()->getId(),
+            'fiscalCode' => $request->get('cpf') ?: $request->get('mei'),
+            'extraFields' => $this->extractExtraFieldsFromRequest($request),
+        ];
+    }
+
+    private function extractExtraFieldsFromRequest(Request $request): array
+    {
+        $extraFields = [];
+
+        $optionalFields = ['social_name', 'full_name', 'public_email', 'private_phone1', 'private_phone2', 'site'];
+
+        foreach ($optionalFields as $field) {
+            if ($request->get($field)) {
+                $extraFields[$field] = $request->get($field);
+            }
+        }
+
+        return $extraFields;
+    }
+
+    private function extractAddressDataFromRequest(Request $request): array
+    {
+        if (!$request->get('postal_code') && !$request->get('street')) {
+            return [];
+        }
+
+        return [
+            'zipcode' => $request->get('postal_code') ?: '',
+            'street' => $request->get('street') ?: '',
+            'number' => $request->get('number') ?: '',
+            'neighborhood' => $request->get('neighborhood') ?: '',
+            'complement' => $request->get('complement_or_reference_point') ?: '',
+            'cityId' => $request->get('address_city'),
+        ];
+    }
+
+    private function createAgentAddress(Agent $agent, array $addressData): void
+    {
+        $agentAddress = new AgentAddress();
+        $agentAddress->setId(Uuid::v4());
+        $agentAddress->setZipcode($addressData['zipcode']);
+        $agentAddress->setStreet($addressData['street']);
+        $agentAddress->setNumber($addressData['number']);
+        $agentAddress->setNeighborhood($addressData['neighborhood']);
+
+        if ($addressData['complement']) {
+            $agentAddress->setComplement($addressData['complement']);
+        }
+
+        if ($addressData['cityId']) {
+            $city = $this->entityManager->getRepository(City::class)->find($addressData['cityId']);
+            if ($city) {
+                $agentAddress->setCity($city);
+            }
+        }
+
+        $agentAddress->setOwner($agent);
+        $this->addressRepository->save($agentAddress);
     }
 
     #[IsGranted(UserRolesEnum::ROLE_USER->value, statusCode: self::ACCESS_DENIED_RESPONSE_CODE)]
