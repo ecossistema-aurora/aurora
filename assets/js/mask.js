2e777195f4b2e4c620f9c5af99dc6b29e3706d41
Talyson Soares <talysonsoares40@gmail.com>
2026-08-17 19:57:55 -0300

    improve and apply masks on inputs (#552)

--- assets/js/mask.js
+++ assets/js/mask.js
@@ -1,4 +1,8 @@
 function applyMask(input, maskFunction) {
+    if (input.value) {
+        input.value = maskFunction(input.value);
+    }
+
     input.addEventListener("input", () => {
         input.value = maskFunction(input.value);
     });
@@ -7,41 +11,61 @@ function applyMask(input, maskFunction) {
 function cpfMask(value) {
     return value
         .replace(/\D/g, '')
+        .substring(0, 11)
         .replace(/(\d{3})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
 }
 
 function phoneMask(value) {
-    return value
-        .replace(/\D/g, '')
-        .replace(/^(\d{2})(\d)/, '($1) $2')
-        .replace(/(\d{1})?(\d{4})(\d{4})$/, '$1 $2-$3');
+    const digits = value.replace(/\D/g, '');
+
+    if (digits.length <= 10) {
+        return digits
+            .replace(/(\d{2})(\d)/, '($1) $2')
+            .replace(/(\d{4})(\d{1,4})/, '$1-$2');
+    }
+
+    return digits
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{1})(\d{4})(\d{1,4})/, '$1 $2-$3')
+        .substring(0, 16);
 }
 
 function cnpjMask(value) {
     return value
         .replace(/\D/g, '')
+        .substring(0, 14)
         .replace(/^(\d{2})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d)/, '$1/$2')
         .replace(/(\d{4})(\d{1,2})$/, '$1-$2');
 }
 
-const cpfInput = document.querySelector('[data-mask="cpf"]');
-const phoneInput = document.querySelectorAll('[data-mask="phone"]');
-const cnpjInput = document.querySelector('[data-mask="cnpj"]');
-
-if (cpfInput) {
-    applyMask(cpfInput, cpfMask);
+function cepMask(value) {
+    return value
+        .replace(/\D/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .substring(0, 9);
 }
 
-phoneInput.forEach(phoneInput => {
-    if (phoneInput) {
-        applyMask(phoneInput, phoneMask);
+document.addEventListener("DOMContentLoaded", () => {
+    const cpfInput = document.querySelector('[data-mask="cpf"]');
+    const phoneInputs = document.querySelectorAll('[data-mask="phone"]');
+    const cnpjInput = document.querySelector('[data-mask="cnpj"]');
+    const cepInput = document.querySelector('[data-mask="cep"]');
+
+    if (cpfInput) applyMask(cpfInput, cpfMask);
+    if (cnpjInput) applyMask(cnpjInput, cnpjMask);
+    if (cepInput) applyMask(cepInput, cepMask);
+
+    phoneInputs.forEach(input => applyMask(input, phoneMask));
+
+    if (cepInput) {
+        applyMask(cepInput, cepMask);
+
+        cepInput.form?.addEventListener('submit', () => {
+            cepInput.value = cepInput.value.replace(/\D/g, '');
+        });
     }
-})
-
-if (cnpjInput) {
-    applyMask(cnpjInput, cnpjMask);
-}
+});
